244b563ec0367747efb2fe60478a4d6cd9dca096
Anshuman Jha <Anshuman Jha@DESKTOP-FOCT3H3>
2021-08-19 22:12:02 +0530

    My 1st Commit...

---
+++ AutomationFramework/src/test/java/generic/IConst.java
@@ -0,0 +1,12 @@
+package generic;
+
+public interface IConst {
+	int ITO=10;
+	String APP_URL="https://demo.actitime.com/login.do";
+	int ETO = 20;
+	String XL_PATH ="./data/input.xlsx";
+	
+	String dURL="http://localhost:4444/wd/hub";
+	String dBrowser="chrome";
+
+}
